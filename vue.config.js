eb531dd1da8f52f621e0bd7e4dbfb290ac77d627
kinggora <jhha09@ajou.ac.kr>
2023-06-16 17:48:31 +0900

    빌드 설정 추가 (webpack)

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,8 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
+  outputDir: "../portal/src/main/resources/static",
+  chainWebpack(config) {
+    config.output.filename("js/[name].js");
+  },
   transpileDependencies: true,
 });
