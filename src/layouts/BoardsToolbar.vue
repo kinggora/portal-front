12709175eeb9a6b6c0ee8b0d5c96f31119b2fea8
kinggora <jhha09@ajou.ac.kr>
2023-10-30 16:41:45 +0900

    BoardsToolbar 생성

--- src/layouts/BoardsToolbar.vue
+++ src/layouts/BoardsToolbar.vue
@@ -1,16 +1,38 @@
 <template>
   <div>
     <v-toolbar title="Portal" color="indigo">
-      <v-btn href="./login">LOGIN</v-btn>
-      <v-btn href="./">LOGOUT</v-btn>
-      <v-btn href="./signup">SIGNUP</v-btn>
+      <div v-if="isAuthenticated">{{ name }} 님</div>
+      <v-btn v-if="!isAuthenticated" @click="moveToLogin">LOGIN</v-btn>
+      <v-btn v-if="!isAuthenticated" @click="moveToSignup">SIGNUP</v-btn>
+      <v-btn v-if="isAuthenticated" @click="logout">LOGOUT</v-btn>
     </v-toolbar>
   </div>
 </template>
 
 <script>
+import router from "@/router";
+import { computed } from "vue";
+import { useStore } from "vuex";
+
 export default {
   name: "BoardsToolbar",
+  setup() {
+    const store = useStore();
+    const isAuthenticated = computed(
+      () => store.state.authStore.isAuthenticated,
+    );
+    const name = computed(() => store.getters["authStore/getMember"]?.name);
+    const logout = () => store.dispatch("authStore/logout");
+    return { isAuthenticated, name, logout };
+  },
+  methods: {
+    moveToLogin() {
+      router.push("/login");
+    },
+    moveToSignup() {
+      router.push("/signup");
+    },
+  },
 };
 </script>
 <style scoped></style>
