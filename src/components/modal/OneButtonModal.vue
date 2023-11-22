4f696e7fa8186e6a0417cdbaaaa29e7509d20fae
kinggora <jhha09@ajou.ac.kr>
2023-11-22 14:53:49 +0900

    modal 디렉터리 생성, One Button Modal 추가

---
+++ src/components/modal/OneButtonModal.vue
@@ -0,0 +1,42 @@
+<template>
+  <v-card class="modal-area">
+    <v-card-text class="text-area">
+      {{ message }}
+    </v-card-text>
+    <v-card-actions justify="center">
+      <v-btn variant="flat" color="indigo" block @click="close">확인</v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: "OneButtonModal",
+  props: {
+    message: {
+      type: String,
+      required: true,
+    },
+  },
+  emits: ["close"],
+  setup(props, { emit }) {
+    const close = () => {
+      emit("close");
+    };
+    return { close };
+  },
+};
+</script>
+
+<style scoped>
+.text-area {
+  margin-bottom: 20px;
+}
+
+.modal-area {
+  padding: 20px 50px 20px 50px;
+  width: max-content;
+  height: fit-content;
+  text-align: center;
+}
+</style>
---
+++ src/components/modal/TwoButtonModal.vue
@@ -0,0 +1,58 @@
+<template>
+  <v-card class="modal-area" variant="tonal" color="indigo">
+    <div class="text-area">
+      {{ question }}
+    </div>
+    <v-card-actions>
+      <v-btn
+        class="mr-2"
+        color="indigo"
+        variant="outlined"
+        width="150px"
+        @click="sendAnswer(false)"
+        >Cancel
+      </v-btn>
+      <v-btn
+        class="ml-2"
+        color="indigo"
+        variant="flat"
+        width="150px"
+        @click="sendAnswer(true)"
+        >OK
+      </v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: "TwoButtonModal",
+  props: {
+    question: {
+      type: String,
+      required: true,
+    },
+  },
+  emits: ["sendAnswer"],
+  setup(props, { emit }) {
+    const sendAnswer = answer => {
+      emit("sendAnswer", answer);
+    };
+    return { sendAnswer };
+  },
+};
+</script>
+
+<style scoped>
+.modal-area {
+  padding: 50px 50px 30px 50px;
+  width: max-content;
+  height: fit-content;
+  text-align: center;
+  background-color: white;
+}
+
+.text-area {
+  margin-bottom: 50px;
+}
+</style>
